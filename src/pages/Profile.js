c3df49a217edf5f8563be883af06195f0295e2f6
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2022-08-22 22:36:31 -0300

    Finalização da página de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,28 +1,42 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 
 function Profile() {
+  const history = useHistory();
+
   const emailInput = localStorage.getItem('user');
+
+  const clearLocalStorage = () => {
+    localStorage.clear();
+
+    history.push('/');
+  };
 
   return (
     <div>
       <Header />
       <span data-testid="profile-email">{emailInput}</span>
-      <button
-        data-testid="profile-done-btn"
-        type="button"
-      >
-        Done Recipes
-      </button>
-      <button
-        data-testid="profile-favorite-btn"
-        type="button"
-      >
-        Favorite Recipes
-      </button>
+      <Link to="/done-recipes">
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+        >
+          Done Recipes
+        </button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+        >
+          Favorite Recipes
+        </button>
+      </Link>
       <button
         data-testid="profile-logout-btn"
         type="button"
+        onClick={ clearLocalStorage }
       >
         Logout
       </button>
