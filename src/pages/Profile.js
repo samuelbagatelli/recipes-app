3b230520c4633a8f6d3fdafe192fa5b801462734
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2022-08-22 21:59:46 -0300

    Resolução do requisito 57

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
+import AppContext from '../context/AppContext';
 
 function Profile() {
+  const {
+    emailInput,
+  } = useContext(AppContext);
+
   return (
     <div>
       <Header />
-      Profile
+      <span data-testid="profile-email">{emailInput}</span>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+      >
+        Logout
+      </button>
     </div>
   );
 }
