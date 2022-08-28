e84efd5933766b77cab9c0db61860dc2398d0a7d
Alec Medeiros <alecmm@outlook.com>
2022-08-27 22:21:32 -0300

    Tela de receita em progresso - SEM OS TESTES

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
-// import './App.css';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Recipes from './pages/Recipes';
 import NotFound from './pages/NotFound';
